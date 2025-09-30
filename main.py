13fc951ef0cc14c197bc0da4acac716b2af128ff
Vishal Ray <vishalray464407@gmail.com>
2025-09-30 13:08:27 +0530

    Logging Added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from src.mlProject.config import configuration
-if __name__ == "__main__":
-    configuration.main()
+#from src.mlProject.config import configuration
+from src.mlProject.logging import logger
+
+logger.info("This is a log message from main.py")
+
